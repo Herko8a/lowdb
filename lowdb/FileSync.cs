ec964844bf165d8969274f0c3be91439caa2498f
Hector Ochoa <hector.ochoa@scanda.com.mx>
2022-03-06 23:08:29 -0600

    Add project files.

---
+++ lowdb/FileSync.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lowdb
+{
+    public class FileSync
+    {
+        public string PathFile { get; set; }
+        Encoding enc = Encoding.UTF8;
+
+
+        public FileSync(string pathFile)
+        {
+            PathFile = Path.Combine(Directory.GetCurrentDirectory(), pathFile);
+        }
+
+        public FileSync(string pathFile, Encoding en)
+        {
+            PathFile = Path.Combine(Directory.GetCurrentDirectory(), pathFile);
+            enc = en;
+        }
+
+        public string read()
+        {
+            string sData = "{}";
+
+            try
+            {
+
+                if (File.Exists(PathFile))
+                    sData = System.IO.File.ReadAllText(PathFile);
+                else
+                    System.IO.File.WriteAllText(PathFile, "{}", Encoding.UTF8);
+            }
+            catch
+            {}
+
+            return sData;
+        }
+
+        public bool write(string data)
+        {
+            bool res = false;
+
+            try
+            {
+                System.IO.File.WriteAllText(PathFile, data, enc);
+                res = true;
+            }
+            catch
+            { }
+
+            return res;
+        }
+
+    }
+
+}
---
+++ lowdb/lowdb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace lowdb
+{
+    public class lowdb<T>
+    {
+        public T data { get; set; }
+        FileSync adapter;
+
+        public lowdb(FileSync fs)
+        {
+            adapter = fs;
+            this.read();
+        }
+
+        T read()
+        {
+            string sdata = adapter.read();
+            data = JsonSerializer.Deserialize<T>(sdata);
+            return data;
+        }
+
+        public bool write()
+        {
+            return adapter.write(JsonSerializer.Serialize(data));
+        }
+
+        public void refresh()
+        {
+            this.read();
+        }
+
+    }
+
+}
